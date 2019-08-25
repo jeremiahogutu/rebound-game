2901a0563a5382fc304ad64fcc92eb6e09d1babd
Jeremiah Ogutu <jogutu@pm.me>
2019-08-25 09:36:48 -0400

    handle sound on mobile devices

--- src/reboundHelper.js
+++ src/reboundHelper.js
@@ -232,3 +232,41 @@ const newGame = () => {
     score.style.backgroundColor = 'rgb(32,128,64)';
     hideSettings()
 };
+
+const initAudio = () => {
+    // load audio files
+    beepX = new Audio('/beepX.mp3');
+    beepY = new Audio('/beepY.mp3');
+    beepPaddle = new Audio('/beepGameOver.mp3');
+    bgMusic = new Audio('/music.mp3')
+
+    //turn off volume
+    beepX.volume = 0;
+    beepY.volume = 0;
+    beepPaddle.volume = 0;
+    beepGameOver.volume = 0;
+    bgMusic.volume = 0;
+
+    //play each file
+    //this grants permission
+    beepX.play();
+    beepY.play();
+    beepPaddle.play();
+    beepGameOver.play();
+    bgMusic.play();
+
+    //pause each file
+    //this stores them in memory for later
+    beepX.pause();
+    beepY.pause();
+    beepPaddle.pause();
+    beepGameOver.pause();
+    bgMusic.pause();
+
+    //set the volume back for next time
+    beepX.volume = 1;
+    beepY.volume = 1;
+    beepPaddle.volume = 1;
+    beepGameOver.volume = 1;
+    bgMusic.volume = 1;
+};
